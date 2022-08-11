4fa296cff230dc31807bc1e69f2aab7cc0a72c1a
chillyunn <dkwhkp0@naver.com>
2022-08-11 11:56:32 +0900

    5장 요구사항1

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -15,29 +15,31 @@ public class HttpRequest {
 
     private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
 
-    public HttpRequest(InputStream in) {
+    public  HttpRequest(InputStream in) {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = br.readLine();
             if (line == null)
                 return;
             String[] tokens = line.split(" ");
+            method = tokens[0];
             int contentLength = 0;
-            String[] url = tokens[1].split("/?");
+            String[] url = tokens[1].split("\\?");
             path = url[0];
-            String[] queryString= url[1].split("&");
-            parameter.put(queryString[0],queryString[1]);
-            while (!line.equals("")) {
-                line = br.readLine();
+            String[] queryString = url[1].split("\\&");
+            for(String val:queryString){
+                parameter.put(val.split("=")[0],val.split("=")[1]);
+            }
+
+            while ((line=br.readLine()) !=null) {
                 log.debug("header: {}", line);
                 tokens = line.split(":");
-                header.put(tokens[0], tokens[1]);
+                header.put(tokens[0],tokens[1].trim());
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
     public String getMethod() {
         return method;
     }
@@ -46,11 +48,11 @@ public class HttpRequest {
         return path;
     }
 
-    public Map<String, String> getHeader() {
-        return header;
+    public String getHeader(String key) {
+        return header.get(key);
     }
 
-    public String getParameter() {
-        return parameter;
+    public String getParameter(String key) {
+        return parameter.get(key);
     }
 }
--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -11,13 +11,16 @@ import java.io.InputStream;
 import static org.junit.Assert.assertEquals;
 
 public class HttpRequestTest {
-    private String testDirectory = "./src/test/resources/";
+    private String testDirectory = "./src/test/java/resources/";
 
     @Test
     public void request_GET() throws Exception{
         InputStream in = new FileInputStream(new File(testDirectory +"Http_GET.txt"));
-
         HttpRequest request = new HttpRequest(in);
+
         assertEquals("GET",request.getMethod());
+        assertEquals("/user/create",request.getPath());
+        assertEquals("keep-alive",request.getHeader("Connection"));
+        assertEquals("javajigi",request.getParameter("userId"));
     }
 }
